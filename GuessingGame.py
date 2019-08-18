c6242544e2c730e9992df57e775ac9a9b31edce3
slugger9066 <hurst.robby@gmail.com>
2019-08-17 20:03:33 -0400

    Add files via upload

--- GuessingGame.py
+++ GuessingGame.py
@@ -15,45 +15,49 @@ def game():
     high = 100
     guess = 1
     random_number = random.randint(low, high)
-    accepted_answers = {'Higher', 'Lower', 'Correct'}
+
+
+    print("############################################################################")
+    print("#                      Backwards Guessing Game                             #")
+    print("############################################################################")
 
     print("I'm going to guess a number between", low, "and", high, "within 5 guesses.")
     print("Please tell me if I am Higher, Lower or Correct.")
-    print("Is", random_number, "the correct number?")
+    print("Guess #", guess,"- Is", random_number, "the correct number?")
 
-    response = input()
+    response = input().lower()
 
     while guess < 5:
-        if response == "Higher":
+        if response == "higher":
             low = random_number + 1
             guess = guess + 1
             random_number = random.randint(low, high)
-            print("Is", random_number, "the correct number?")
-            response = input()
-        elif response == "Lower":
+            print("Guess #", guess, "- Is", random_number, "the correct number?")
+            response = input().lower()
+        elif response == "lower":
             high = random_number - 1
             guess = guess + 1
             random_number = random.randint(low, high)
-            print("Is", random_number, "the correct number?")
-            response = input()
-        elif response == "Correct":
+            print("Guess #", guess, "- Is", random_number, "the correct number?")
+            response = input().lower()
+        elif response == "correct":
             print("I guessed correctly and it took", guess, "guesses")
             play_again()
-        elif response != ["Higher", "Lower", "Correct"]:
-            print("Your answer is not Higher, Lower or Correct, please try again.")
-            print("Is", random_number, "the correct number?")
-            response = input()
-    else:
-        print("I didn't guess correctly within", guess, "guesses")
-        play_again()
+        elif response != ["higher", "lower", "correct"]:
+            print("I didn't understand your answer, please answer with Higher, Lower or Correct, please try again.")
+            print("Guess #", guess, "- Is", random_number, "the correct number?")
+            response = input().lower()
+        if guess == 5:
+            print("I didn't guess correctly within", guess, "guesses.")
+            play_again()
 
 
 def play_again():
     while True:
-        answer = input("Would you like to play again? (Y/N)")
-        if answer == "Y":
+        answer = input("Would you like to play again? (Y/N)").lower()
+        if answer == "y":
             game()
-        elif answer == "N":
+        elif answer == "n":
             exit()
         else:
             print("I didn't recognize your answer, please try again.")
